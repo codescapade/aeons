4295e6107e09f7651de09723ae735b6b80f44d4c
Jurien Meerlo <codescapade@gmail.com>
2022-03-09 20:50:20 +0000

    updated some tests.

--- tests/game/basic/src/scenes/RenderScene.hx
+++ tests/game/basic/src/scenes/RenderScene.hx
@@ -12,7 +12,7 @@ class RenderScene extends Scene {
   public override function init() {
     addSystem(RenderSystem).init();
 
-    var bunnyImg = assets.getImage('bunny');
+    var atlas = assets.loadAtlas('atlas');
 
     var eCam = addEntity(Entity);
     eCam.addComponent(CTransform).init();
@@ -20,6 +20,6 @@ class RenderScene extends Scene {
 
     var eSprite = addEntity(Entity);
     eSprite.addComponent(CTransform).init({ x: 100, y: 100 });
-    // eSprite.addComponent(CSprite).init({ image: bunnyImg });
+    eSprite.addComponent(CSprite).init({ atlas: atlas, frameName: 'bunny' });
   }
 }
--- tests/game/bunnies/khafile.js
+++
@@ -1,7 +0,0 @@
-let project = new Project('Bunnies');
-
-project.addAssets('assets');
-project.addSources('src');
-project.addSources('../../../');
-
-resolve(project);
